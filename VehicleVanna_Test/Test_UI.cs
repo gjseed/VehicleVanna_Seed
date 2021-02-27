8cb737aa2fbb939d56d9d18ab9234a4283885ed6
glinda seed <glinda.seed@student.centralia.edu>
2021-02-26 19:40:11 -0800

    fixed the url

--- VehicleVanna_Test/Test_UI.cs
+++ VehicleVanna_Test/Test_UI.cs
@@ -17,8 +17,9 @@ namespace VehicleVanna_Test
             VehicleEnum.AutoEnum newAuto = new VehicleEnum.AutoEnum(MakeBx.Text, ModelBx.Text, YearBx.Text,
                 (Int32)ListPBx.Value, VehicleTBx.SelectedIndex.ToString(), EmailBx.Text, FnameBx.Text, LnameBx.Text);
             string url = "http://localhost:7071/api/Function1";
+            string url2 = "https://vehiclevannaseed.azurewebsites.net/api/Function1?code=Zgz1B2vIizC1eorWjPzT2sLE4oNauPSBcNL5l7YLi0IMYtenaD97ZA==";
             var client = new HttpClient();
-            HttpResponseMessage response = await client.PostAsJsonAsync(url, newAuto);
+            HttpResponseMessage response = await client.PostAsJsonAsync(url2, newAuto);
             this.Hide();
             MessageBox.Show(response.Content.ReadAsStringAsync().Result.ToString());
             this.Show();
